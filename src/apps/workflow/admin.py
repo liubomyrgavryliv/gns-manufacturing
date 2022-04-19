105628af62577a7d89a2b16b7c8459e8c57920ec
liubomyr.gavryliv <liubomyr.gavryliv@gmail.com>
2022-04-19 22:55:50 +0200

    expanded admin

--- src/apps/workflow/admin.py
+++ src/apps/workflow/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import TextInput, Textarea
 
 from .models import core as core_models
 from .models import stage as stage_models
+from . import inlines as inlines
 
 
 class WfModelListAdmin(admin.ModelAdmin):
@@ -10,6 +13,8 @@ class WfModelListAdmin(admin.ModelAdmin):
         'id',
         'name',
     ]
+    
+    list_editable = ['name',]
     
     search_fields = [
         'name',
@@ -24,9 +29,11 @@ class WfJobStatusListAdmin(admin.ModelAdmin):
         'name',
     ]
     
+    list_display_links = ['name',]
+    
     search_fields = [
         'name',
-    ]
+    ]    
     
     
     
@@ -37,9 +44,11 @@ class WfConfigurationListAdmin(admin.ModelAdmin):
         'name',
     ]
     
+    list_display_links = ['name',]
+    
     search_fields = [
         'name',
-    ]
+    ]    
 
 
 
@@ -50,9 +59,11 @@ class WfFireclayTypeListAdmin(admin.ModelAdmin):
         'name',
     ]
     
+    list_display_links = ['name',]
+    
     search_fields = [
         'name',
-    ]
+    ]    
     
     
     
@@ -63,9 +74,11 @@ class WfFrameTypeListAdmin(admin.ModelAdmin):
         'name',
     ]
     
+    list_display_links = ['name',]
+    
     search_fields = [
         'name',
-    ]
+    ]    
 
 
 
@@ -76,9 +89,11 @@ class WfGlazingTypeListAdmin(admin.ModelAdmin):
         'name',
     ]
     
+    list_display_links = ['name',]
+    
     search_fields = [
         'name',
-    ]
+    ]    
     
     
     
@@ -89,23 +104,12 @@ class WfPriorityListAdmin(admin.ModelAdmin):
         'name',
     ]
     
-    search_fields = [
-        'name',
-    ]
-
-
-
-class WfQualityControlListAdmin(admin.ModelAdmin):
-    
-    list_display = [
-        'id',
-        'name',
-    ]
+    list_display_links = ['name',]
     
     search_fields = [
         'name',
-    ]
-    
+    ]    
+
     
     
 class WfPaymentListAdmin(admin.ModelAdmin):
@@ -115,9 +119,11 @@ class WfPaymentListAdmin(admin.ModelAdmin):
         'name',
     ]
     
+    list_display_links = ['name',]
+    
     search_fields = [
         'name',
-    ]
+    ]    
     
     
     
@@ -128,9 +134,11 @@ class WfBendingStationListAdmin(admin.ModelAdmin):
         'name',
     ]
     
+    list_display_links = ['name',]
+    
     search_fields = [
         'name',
-    ]
+    ]    
     
     
     
@@ -141,9 +149,11 @@ class WfWeldingStationListAdmin(admin.ModelAdmin):
         'name',
     ]
     
+    list_display_links = ['name',]
+    
     search_fields = [
         'name',
-    ]
+    ]    
     
     
     
@@ -151,10 +161,13 @@ class WfDFXVersionControlLogAdmin(admin.ModelAdmin):
     
     list_display = [
         'id',
+        
         'order',
         'stage',
         'status',
-    ]
+    ]    
+    
+    list_display_links = ['order', 'stage', 'status',]
     
     list_select_related = [
         'order',
@@ -174,26 +187,82 @@ class WfDFXVersionControlLogAdmin(admin.ModelAdmin):
     
 class WfOrderLogAdmin(admin.ModelAdmin):
     
+    def get_inlines(self, request, obj):
+        return super().get_inlines(request, obj)
+    
+    # inlines = [
+    #     inlines.WfDFXVersionControlLogInline,
+    #     inlines.WfCutLogInline,
+    #     inlines.WfBendLogInline,
+    #     inlines.WfWeldLogInline,
+    #     inlines.WfLocksmithLogInline,
+    #     inlines.WfGlassLogInline,
+    #     inlines.WfQualityControlLogInline,
+    #     inlines.WfFinalProductLogInline,
+    # ]
+    
     list_display = [
         'id',
+        
         'model',
         'configuration',
+        'fireclay_type',
+        'glazing_type',
+        'frame_type',
+        'priority',
+        
+        'note',
+        
+        'delivery',
+        'mobile_number',
+        'email',
+        'payment',
+        
+        'start_manufacturing',
+        'start_date',
+        'deadline_date',
     ]
     
+    list_display_links = ['model', 'configuration', 'fireclay_type',]
+    
+    list_filter = ['model', 'configuration', 'fireclay_type', 'glazing_type', 'priority',]
+        
     list_select_related = [
         'model',
         'configuration',
+        'fireclay_type',
+        'glazing_type',
+        'frame_type',
+        'priority',
     ]
     
     search_fields = [
-        'id',
-        'model',
-        'configuration',
+        'model__name',
+        'configuration__name',
     ]
     
     ordering = ['-created_at',]
     
+    fieldsets = (
+        (None, {
+        'fields': (
+            ('model', 'configuration', 'fireclay_type', 'glazing_type', 'frame_type', 'priority',),
+            ('note',),
+            'delivery', 'mobile_number', 'email', 'payment',
+            'start_manufacturing', 'start_date', 'deadline_date',
+            ),
+        'classes': ('wide', 'extrapretty'),
+        }),
+    )
     
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':'20'})},
+        models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
+    }
+    
+
+admin.site.site_header = 'Gavryliv&Sons'
+admin.site.index_title = 'Адміністрація'
 
 admin.site.register(core_models.WfModelList, WfModelListAdmin)
 admin.site.register(core_models.WfJobStatusList, WfJobStatusListAdmin)
@@ -202,7 +271,6 @@ admin.site.register(core_models.WfFireclayTypeList, WfFireclayTypeListAdmin)
 admin.site.register(core_models.WfFrameTypeList, WfFrameTypeListAdmin)
 admin.site.register(core_models.WfGlazingTypeList, WfGlazingTypeListAdmin)
 admin.site.register(core_models.WfPriorityList, WfPriorityListAdmin)
-admin.site.register(core_models.WfQualityControlList, WfQualityControlListAdmin)
 admin.site.register(core_models.WfPaymentList, WfPaymentListAdmin)
 admin.site.register(core_models.WfBendingStationList, WfBendingStationListAdmin)
 admin.site.register(core_models.WfWeldingStationList, WfWeldingStationListAdmin)
