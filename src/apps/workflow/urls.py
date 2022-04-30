d0d4614141a786632f53884da040905373ffb9f2
Liubomyr Gavryliv <liubomyr.gavryliv@gmail.com>
2022-04-30 14:22:47 +0200

    login finished; dfx_log_list WIP

--- src/apps/workflow/urls.py
+++ src/apps/workflow/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-from . import views as views
+from .views import WfLogListView
 
 app_name = 'workflow'
 
     
 urlpatterns = [
     path('', TemplateView.as_view(template_name='main/home.html'), name='home'),
+    path('logs/', WfLogListView.as_view(), name='logs'),
 ]
--- src/apps/workflow/views.py
+++ src/apps/workflow/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.db.models import Q, Count
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models.core import WfDFXVersionControlLog, WfOrderLog
+
+
+class WfLogListView(LoginRequiredMixin, ListView):
+
+    http_method_names = ['get', 'head', 'options', 'trace']
+
+    queryset = WfOrderLog.objects.filter(Q(start_manufacturing=True) & Q(dfx_logs__isnull=True))
+    ordering = ['priority__id', '-start_date', 'deadline_date',]
+
+    context_object_name = 'logs'
+    
+    template_name = 'workflow/dfx_log_list.html'
