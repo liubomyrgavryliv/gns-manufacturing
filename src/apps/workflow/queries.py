4bad8b1c1d733271dfb6ca171908b7d993a309e7
liubomyr.gavryliv <liubomyr.gavryliv@gmail.com>
2022-06-08 00:48:49 +0200

    added current stage, differentiate templates

--- src/apps/workflow/queries.py
+++ src/apps/workflow/queries.py
@@ -1,27 +1,21 @@
-from django.db.models import Q, OuterRef, Case, When, Value
+from django.db.models import Q, OuterRef, Case, When, Value, Exists, Max, Subquery
 
-from .models.core import WfOrderLog
+from .models.core import WfCutLog, WfOrderLog, WfFinalProductLog, WfLocksmithLog, WfGlassLog, WfBendLog, \
+WfDXFVersionControlLog, WfWeldLog, WfQualityControlLog
 
 
 def annotate_current_stage():
     
-    return WfOrderLog.objects.filter(Q(dxf_logs__isnull=False) & Q(dxf_logs__stage__id=2) & Q(dxf_logs__status__id=1) |
-                                        Q(cut_logs__isnull=False) & Q(cut_logs__stage__id=2) & Q(dxf_logs__status__id=1) |
-                                        Q(bend_logs__isnull=False) & Q(bend_logs__stage__id=2) & Q(dxf_logs__status__id=1) |
-                                        Q(weld_logs__isnull=False) & Q(weld_logs__stage__id=2) & Q(dxf_logs__status__id=1) |
-                                        Q(locksmith_logs__isnull=False) & Q(locksmith_logs__stage__id=2) & Q(dxf_logs__status__id=1) |
-                                        Q(glass_logs__isnull=False) & Q(glass_logs__stage__id=2) & Q(glass_logs__status__id=1) |
-                                        Q(quality_logs__isnull=False) & Q(quality_logs__stage__id=2) & Q(quality_logs__status__id=1) |
-                                        Q(final_logs__isnull=False) & Q(final_logs__stage__id=2) & Q(final_logs__status__id=1)
-                              ) \
-                             .annotate(current_stage=Case(
-                                When(Q(final_logs__isnull=False) & Q(final_logs__stage__id=2) & Q(final_logs__status__id=1), then=Value('Фінальний продукт')),
-                                When(Q(quality_logs__isnull=False) & Q(quality_logs__stage__id=2) & Q(quality_logs__status__id=1), then=Value('Контроль якості')),
-                                When(Q(glass_logs__isnull=False) & Q(glass_logs__stage__id=2) & Q(glass_logs__status__id=1), then=Value('Скління')),
-                                When(Q(locksmith_logs__isnull=False) & Q(locksmith_logs__stage__id=2) & Q(dxf_logs__status__id=1), then=Value('Слюсарські роботи')),
-                                When(Q(weld_logs__isnull=False) & Q(weld_logs__stage__id=2) & Q(dxf_logs__status__id=1), then=Value('Зварювання')),
-                                When(Q(bend_logs__isnull=False) & Q(bend_logs__stage__id=2) & Q(dxf_logs__status__id=1), then=Value('Гнуття')),
-                                When(Q(cut_logs__isnull=False) & Q(cut_logs__stage__id=2) & Q(dxf_logs__status__id=1), then=Value('Порізка')),
-                                When(Q(dxf_logs__isnull=False) & Q(dxf_logs__stage__id=2) & Q(dxf_logs__status__id=1), then=Value('Перевірка DXF версій')),
-                                default=Value('Очікує виконання')
-                            ))
+    return WfOrderLog.objects.annotate(
+                                 current_stage=Case(
+                                    When(Exists(WfFinalProductLog.objects.filter(Q(stage__id=2) & Q(status__id=1) & Q(order=OuterRef('id')))), then=Value('Фінальний продукт')),
+                                    When(Exists(WfQualityControlLog.objects.filter(Q(stage__id=2) & Q(status__id=1) & Q(order=OuterRef('id')))), then=Value('Контроль якості')),
+                                    When(Exists(WfGlassLog.objects.filter(Q(stage__id=2) & Q(status__id=1) & Q(order=OuterRef('id')))), then=Value('Скління')),
+                                    When(Exists(WfLocksmithLog.objects.filter(Q(stage__id=2) & Q(status__id=1) & Q(order=OuterRef('id')))), then=Value('Слюсарські роботи')),
+                                    When(Exists(WfWeldLog.objects.filter(Q(stage__id=2) & Q(status__id=1) & Q(order=OuterRef('id')))), then=Value('Зварювання')),
+                                    When(Exists(WfBendLog.objects.filter(Q(stage__id=2) & Q(status__id=1) & Q(order=OuterRef('id')))), then=Value('Гнуття')),
+                                    When(Exists(WfCutLog.objects.filter(Q(stage__id=2) & Q(status__id=1) & Q(order=OuterRef('id')))), then=Value('Порізка')),
+                                    When(Exists(WfDXFVersionControlLog.objects.filter(Q(stage__id=2) & Q(status__id=1) & Q(order=OuterRef('id')))), then=Value('Перевірка DXF версій')),
+                                    default=Value('Очікує виконання')
+                                 )
+                                )
